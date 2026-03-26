831a54b179f1f330ff18dc5cd96f6209558b807f
Arthur Nguyen <ngocn.bnwz@gmail.com>
2026-03-26 18:38:22 -0500

    Added public & private subnets accross two availability zones

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+# Main VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
@@ -7,5 +8,61 @@ resource "aws_vpc" "main" {
     Name = "microservices-vpc"
     Environment = "dev"
     Project = "aws-microservices-platform"
+  }
+}
+
+# Public Subnets
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                  = "microservicces-public-1"
+    Environment           = "dev"
+    Project               = "aws-microservices-platform"
+    Type                  = "public"
+  }
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "microservices-public-2"
+    Environment = "dev"
+    Project     = "aws-microservices-platform"
+    Type        = "public"
+  }
+}
+
+# Private Subnets
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name        = "microservices-private-1"
+    Environment = "dev"
+    Project     = "aws-microservices-platform"
+    Type        = "private"
+  }
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name        = "microservices-private-2"
+    Environment = "dev"
+    Project     = "aws-microservices-platform"
+    Type        = "private"
   }
 }
